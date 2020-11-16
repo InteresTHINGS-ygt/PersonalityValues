38a2b53f34502bb6df0c745263a1740bae70b138
InteresTHINGS-ygt <69125797+InteresTHINGS-ygt@users.noreply.github.com>
2020-11-16 22:20:12 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,993 @@
+questions = [
+    {
+        "question": "I like to have some exictement in my life.",
+        "effect": {
+            "a": 5,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I like to lead or influence others.",
+        "effect": {
+            "a": 10,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I prefer to reflect before acting.",
+        "effect": {
+            "a": -5,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I don’t like to take control of situations.",
+        "effect": {
+            "a": -10,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I tend to be very sluggish.",
+        "effect": {
+            "a": -20,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I have a very leisurely lifestyle.",
+        "effect": {
+            "a": -15,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I do things with a superhuman speed.",
+        "effect": {
+            "a": 20,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "It’s very easy to take action for me.",
+        "effect": {
+            "a": 15,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I easily work in a group.",
+        "effect": {
+            "a": 0,
+            "e": 10,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I have an easier time making friends than others.",
+        "effect": {
+            "a": 0,
+            "e": 5,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I tend to have really high spirits.",
+        "effect": {
+            "a": 0,
+            "e": 20,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I like to engage with people at all costs.",
+        "effect": {
+            "a": 0,
+            "e": 15,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I prefer keeping to myself.",
+        "effect": {
+            "a": 0,
+            "e": -10,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I think alone time is a luxury.",
+        "effect": {
+            "a": 0,
+            "e": -15,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I never feel boredom even with no socializing.",
+        "effect": {
+            "a": 0,
+            "e": -20,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I can be a bit hard to get to know.",
+        "effect": {
+            "a": 0,
+            "e": -5,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I give without expecting anything in return.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 15,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "Others’ feelings become my own feelings.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 20,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I am easily moved by others’ sufferings.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 10,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I take care of myself before others.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": -10,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I can sometimes express cruelty to others.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": -15,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I may have a hard time trusting some people.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": -5,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "Others tell me that I lack empathy.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": -20,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I tend to see people as decent and trustworthy.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 5,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I respect the morality.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 10,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I prefer cooperation over competition.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 5,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I make myself the center of attention.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": -5,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I have an easy time deceiving others.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": -20,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I don’t mind stepping on others to get ahead.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": -15,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I see others as my opponents.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": -10,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "Politeness is very important for me.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 15,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I internally criticize myself if I get rude.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 20,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I live to work hard. ",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 20,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I sometimes don't work hard enough.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": -5,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I do the bare minimum most of the time.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": -10,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I try to excel in what I do.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 15,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "Others see me as a slacker.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": -20,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "Discipline makes me feel trapped.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": -15,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I see the value in determination.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 5,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I discipline myself to get things done.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 10,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I have a chaotic spirit.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": -20,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I can be forgetful about things.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": -15,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I have a certain sense of order. ",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 5,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I try to do things by the book.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 10,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I feel the need to organize everything.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 20,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I tend to be meticulous and detail-oriented.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 15,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I am not very constrained.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": -5,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I sometimes act without thinking.,
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": -10,
+			"w": 0,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I am paralyzed by anxiety.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 15,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I can calm myself down.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": -5,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I easily take big risks.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": -15,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I never feel depressed or anxious.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": -20,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I am satisfied by myself.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": -10,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "There are many things that I don’t like about myself.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 10,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I don’t feel confident about myself.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 5,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I run around with panic most of the time.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 20,
+			"v": 0,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I tend to have good self-control.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": -10,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I don’t have many cravings at all.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": -15,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I can’t control my cravings.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 10,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "When I become angry, it turns into rage.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 15,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I tend to complain about things.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 5,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I become destructive under typical stress.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 20,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I stay calm even in the highest tension.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": -20,
+			"n": 0,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I can be calm in stressful situations.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": -5,
+			"n": 0,
+			"x": 0
+        }
+    },
+	{
+        "question": "I am always eager to solve problems.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 15,
+			"x": 0
+        }
+    },
+	    {
+        "question": "I enjoy theoretical discussions.",
+        "effect": {
+            "a": 0,
+            "e": 0,
+            "c": 0,
+            "p": 0,
+			"i": 0,
+			"o": 0,
+			"w": 0,
+			"v": 0,
+			"n": 10,
+			"x": 0
+        }
+    }
+
+];
