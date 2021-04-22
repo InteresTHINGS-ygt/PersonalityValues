0b8a0096804a475b91bc475981a7f4556742378b
InteresTHINGS-ygt <69125797+InteresTHINGS-ygt@users.noreply.github.com>
2021-04-22 22:08:02 +0300

    Update youtubers.js

--- youtubers.js
+++ youtubers.js
@@ -1,6 +1,6 @@
 youtubers = [
     {
-        "name": "Grant Sanderson",
+        "name": "Grant Sanderson (3Blue1Brown)",
         "stats": {
             "a": 30,
             "e": 50,
@@ -15,7 +15,7 @@ youtubers = [
         }
     },
     {
-        "name": "Drake Anthony",
+        "name": "Drake Anthony (styropyro)",
         "stats": {
             "a": 50,
             "e": 60,
@@ -45,7 +45,7 @@ youtubers = [
         }
     },
 	{
-        "name": "Mitchell Moffit",
+        "name": "Mitchell Moffit (AsapSCIENCE)",
         "stats": {
             "a": 70,
             "e": 60,
@@ -60,7 +60,7 @@ youtubers = [
         }
     },
 	{
-        "name": "Enderman",
+        "name": "Andrew (Enderman)",
         "stats": {
             "a": 50,
             "e": 50,
@@ -75,7 +75,7 @@ youtubers = [
         }
     },
 	{
-        "name": "Oliver Brotherhood",
+        "name": "Oliver Brotherhood (Mumbo Jumbo)",
         "stats": {
             "a": 30,
             "e": 50,
@@ -88,5 +88,80 @@ youtubers = [
 			"n": 100,
 			"x": 70
         }
-    }
+    },
+	{
+        "name": "Craig (Colon)",
+        "stats": {
+            "a": 20,
+            "e": 30,
+            "c": 30,
+            "p": 20,
+			"s": 20,
+			"o": 20,
+			"w": 80,
+			"v": 50,
+			"n": 100,
+			"x": 80
+        }
+    },
+	{
+        "name": "Frank James",
+        "stats": {
+            "a": 10,
+            "e": 20,
+            "c": 80,
+            "p": 70,
+			"s": 90,
+			"o": 60,
+			"w": 50,
+			"v": 30,
+			"n": 60,
+			"x": 70
+        }
+    },
+	{
+        "name": "Milan Struyf (CubeHead)",
+        "stats": {
+            "a": 100,
+            "e": 100,
+            "c": 70,
+            "p": 60,
+			"s": 70,
+			"o": 50,
+			"w": 50,
+			"v": 20,
+			"n": 60,
+			"x": 100
+        }
+    },
+	{
+        "name": "Daniel Karnaukh (Derpy Cuber)",
+        "stats": {
+            "a": 50,
+            "e": 60,
+            "c": 60,
+            "p": 60,
+			"s": 50,
+			"o": 10,
+			"w": 50,
+			"v": 50,
+			"n": 100,
+			"x": 100
+        }
+    },
+	{
+        "name": "Krisztofer Kelemen (Krisz)",
+        "stats": {
+            "a": 80,
+            "e": 70,
+            "c": 80,
+            "p": 80,
+			"s": 50,
+			"o": 20,
+			"w": 0,
+			"v": 0,
+			"n": 100,
+			"x": 90
+        }
+    },
 ];
